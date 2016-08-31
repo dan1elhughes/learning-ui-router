7be54428de6d0f4d5234f101d5462b16d9407dec
Dan Hughes <danhughes.dh+github@gmail.com>
2016-08-31 15:45:44 +0100

    Fix babel/rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,13 @@
+import babel from 'rollup-plugin-babel';
 import uglify from 'rollup-plugin-uglify';
 
 export default {
 	entry: 'app/main.js',
-	dest: 'dist/main.js',
-	sourceMap: 'inline',
+	dest: 'public/js/dist.js',
 	plugins: [
+		babel({
+			exclude: 'node_modules/**'
+		}),
 		uglify()
 	]
 };
